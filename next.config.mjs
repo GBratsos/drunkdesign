87bc580592a011dfc79d341a9652018ace9000b7
George <geobratsos@gmail.com>
2025-06-19 18:02:52 +0300

    more packages

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  distDir: 'build',
+  images: {
+    unoptimized: true,
+    minimumCacheTTL: 31536000,
+  },
+};
 
 export default nextConfig;
